6b42b661bfb3e1ae705746b924031b654dd506a8
Beril Bolukbasi <beril.bls23107@gmail.com>
2025-10-28 12:29:27 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Basic Home Page',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
+      ),
+      home: const MyHomePage(title: 'Basic Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+
+    var ekranBilgisi = MediaQuery.of(context);
+    final double ekranYuksekligi = ekranBilgisi.size.height;
+    final double ekranGenisligi = ekranBilgisi.size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top:ekranYuksekligi/20, bottom:ekranGenisligi/100),
+                child: SizedBox(
+                    width: ekranGenisligi/4,
+                    child: Image.asset("image/stevejobs@2x.png")
+                ),
+              ),
+              Text("Steve Jobs",
+                style: TextStyle(
+                  color: Colors.redAccent,
+                  fontWeight: FontWeight.bold,
+                  fontSize: ekranGenisligi/25,
+                ),
+              ),
+              const SizedBox(height: 100),
+              Padding(
+                padding: EdgeInsets.only(left: ekranGenisligi/100, right: ekranGenisligi/100),
+                child: Text("Dünyayı değiştiricek insanlar, onu değiştirebiliceklerini düşünecek kadar çılgın olanlardır.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: ekranGenisligi/25,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 100),
+              Padding(
+                padding: EdgeInsets.only(bottom: ekranYuksekligi/100),
+                child: SizedBox(
+                  width: ekranGenisligi/2,
+                  height: ekranYuksekligi/15,
+                  child: ElevatedButton(
+                    onPressed: (){
+                      print("İlham verildi");
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.redAccent,
+                      foregroundColor: Colors.white,
+                    ),
+                    child: Text("İLHAM VER",style: TextStyle(fontSize: ekranGenisligi/25),),
+                  ),
+                ),
+              ),
+            ],
+          )
+      )
+    );
+  }
+}
